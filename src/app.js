1eb29010a8b6261f1f55e0e92d4677c76f09f007
devellybutton <leegr0106@gmail.com>
2026-01-09 21:38:52 +0900

    CORS 설정 추가

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500"],
+    origin: [
+      "http://127.0.0.1:5500",
+      "http://localhost:5500",
+      "https://flow-assignment-123.s3.ap-northeast-2.amazonaws.com/index.html",
+    ],
     methods: ["GET", "POST", "PATCH", "DELETE"],
     allowedHeaders: ["Content-Type"],
   })
